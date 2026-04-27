c83b7b11066e10bc658b443ef8260202432ec154
zzjingzhang <1024473568@qq.com>
2026-04-27 15:06:38 +0800

    .80723971410304:a1694f7c166b9ca4c4850088e8037b0a_69ef0346c85b919211c0efca.69ef0839c85b919211c0f279.69ef0839e8b88f4e7ea274e1:Trae CN.T(2026/4/27 14:54:49)

--- future-data-visualization/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-
-export const routes: Routes = [
-  { path: '', component: DashboardComponent, pathMatch: 'full' },
-  { path: '**', redirectTo: '' }
-];
