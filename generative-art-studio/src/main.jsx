7cd74d1635f06f7a780114e4249a43f339afc104
zzjingzhang <1024473568@qq.com>
2026-04-28 22:09:47 +0800

    .80723971410304:07794273d1992d0677e1ce548b77f780_69f0ac6966ffbeb9afa56911.69f0b89766ffbeb9afa56a89.69f0b897a2376623ee423b7b:Trae CN.T(2026/4/28 21:39:35)

--- generative-art-studio/src/main.jsx
+++ generative-art-studio/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import 'primereact/resources/themes/lara-light-cyan/theme.css'
 import './index.css'
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
