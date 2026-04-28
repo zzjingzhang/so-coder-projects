3eb8da02347b57fb4d6e4fa5cdaf1dd5f9f21ee2
zzjingzhang <1024473568@qq.com>
2026-04-28 15:58:29 +0800

    .80723971410304:c987c8d61e28ca52c502ef502f9a939d_69efa11566ffbeb9afa55cff.69f050ca66ffbeb9afa56437.69f050caa2376623ee423b67:Trae CN.T(2026/4/28 14:16:42)

--- skateboard-tracker/vite.config.ts
+++ skateboard-tracker/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
